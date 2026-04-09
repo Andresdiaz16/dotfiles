f282a87f296b0a05444abf97772a3c38118b219b
Andres Diaz <45214848+Andresdiaz16@users.noreply.github.com>
2026-04-08 18:56:04 -0600

    added more binds for telescope

--- nvim/plugin/05_telescope.lua
+++ nvim/plugin/05_telescope.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>ss', builtin.grep_string, { desc = 'Telescope strings' })
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>pds', builtin.lsp_document_symbols, { desc = 'Telescope document symbols' })
+vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = 'Telescope document symbols' })
 vim.keymap.set('n', '<leader>vrr', builtin.lsp_references, { desc = 'Lists LSP references for word under the cursor' })
 vim.keymap.set('n', '<leader>ld', '<CMD> Telescope diagnostics bufnr=0<CR>', { desc = 'List diagnostics'})
 vim.keymap.set('n', '<leader>lpd', builtin.diagnostics, { desc = 'Lists Diagnostics for all buffers' })
